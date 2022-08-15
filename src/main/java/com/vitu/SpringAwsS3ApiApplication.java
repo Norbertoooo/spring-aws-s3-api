5e33a159d03db0287d6c5e0d821de0851d126fe6
Vítor Norberto <vithor.norberto@gmail.com>
2022-08-15 00:42:24 -0300

    Inicia projeto

---
+++ src/main/java/com/vitu/SpringAwsS3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsS3ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsS3ApiApplication.class, args);
+    }
+
+}
